8328fe84f44b0c57dff363b4e9e2122aa6041a44
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-08 13:06:32 +0530

    Added perimeterFunction in EquilateralPyramid.java

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -7,6 +7,7 @@ class EquilateralPyramid extends Shape implements Volume {
         this.baseSide = baseSide;
         this.height = height;
     }
+
     @Override
     double calculateArea() {
         double baseArea = baseSide * baseSide;
@@ -14,8 +15,14 @@ class EquilateralPyramid extends Shape implements Volume {
         double lateralArea = 2 * baseSide * slantHeight;
         return baseArea + lateralArea;
     }
+
     @Override
     public double calculateVolume() {
         return (1.0 / 3.0) * baseSide * baseSide * height;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 4 * baseSide;
     }
 }
