c688927dbf5ff943cbb1f55755ec28b5291418be
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-08 13:07:12 +0530

    Created Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+// Rectangle.java
+class Rectangle extends Shape {
+    private double length, width;
+
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        this.length = length;
+        this.width = width;
+    }
+}
