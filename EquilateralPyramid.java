bb48f61e8300688d7fc3df6c33cb55e2800ac004
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-08 13:05:18 +0530

    Added areaFunction in EquilateralPyramid.java

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -7,4 +7,11 @@ class EquilateralPyramid extends Shape implements Volume {
         this.baseSide = baseSide;
         this.height = height;
     }
+    @Override
+    double calculateArea() {
+        double baseArea = baseSide * baseSide;
+        double slantHeight = Math.sqrt((baseSide / 2) * (baseSide / 2) + height * height);
+        double lateralArea = 2 * baseSide * slantHeight;
+        return baseArea + lateralArea;
+    }
 }
