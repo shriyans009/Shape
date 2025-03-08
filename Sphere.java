0bf283d21acfbee5f7bf04d4bbb14272ed74466c
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-08 13:11:56 +0530

    Added volumeFunction in Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -10,4 +10,8 @@ class Sphere extends Shape implements Volume {
     double calculateArea() {
         return 4 * Math.PI * radius * radius;
     }
+    @Override
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
 }
